b301778c4eafa3470fe7727b6183a6c12ba078cd
Matthew <22198949+MafewTM@users.noreply.github.com>
2024-03-10 17:12:44 -0400

    feat(ui): remove sass

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createTheme, MantineProvider } from '@mantine/core';
 import App from './App';
-import './index.scss';
 import "@mantine/core/styles.css";
 
 export const theme = createTheme({
