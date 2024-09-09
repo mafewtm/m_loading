1259ac2bca859e76b92e441a727d10930088a8cd
Matthew <22198949+MafewTM@users.noreply.github.com>
2024-09-09 19:54:06 -0400

    refactor: simplify root element casting

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -7,7 +7,7 @@ import '@mantine/core/styles.css';
 import '@mantine/carousel/styles.css';
 import '@mantine/nprogress/styles.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <MantineProvider defaultColorScheme='dark'>
       <App />
